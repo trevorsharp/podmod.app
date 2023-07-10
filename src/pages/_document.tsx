ca9addc686a295dcf6466410787eac2c58913642
Trevor Sharp <sharp.trevor@me.com>
2023-07-10 22:08:11 +0000

    Change theme color based on dark mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,11 +7,12 @@ const Document = () => (
         name="description"
         content="Modify any podcast feed with custom filters, artwork, titles, and more!"
       />
-      <meta name="theme-color" content="#00A6FB" />
+      <meta name="theme-color" content="#00A6FB" media="(prefers-color-scheme: light)" />
+      <meta name="theme-color" content="#0582CA" media="(prefers-color-scheme: dark)" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="112x112" href="/apple-touch-icon.png" />
     </Head>
-    <body className="bg-podmod">
+    <body className="bg-podmod dark:bg-podmod-dark">
       <main className="bg-white text-neutral-800 dark:bg-neutral-900 dark:text-white">
         <Main />
         <NextScript />
