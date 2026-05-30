8f2478ef2cf64a2cf88c5d1e7aac85d51bd9779f
Trevor Sharp <sharp.trevor@me.com>
2026-05-30 19:43:01 +0000

    Add LOG_REQUESTS env variable

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,9 @@
 const UI_FOLDER_PATH = "./static";
 const PORT = Number(process.env.PORT ?? 3000);
+const LOG_REQUESTS = process.env.LOG_REQUESTS === "true";
 
 export default {
+  LOG_REQUESTS,
   PORT,
   UI_FOLDER_PATH,
 };
